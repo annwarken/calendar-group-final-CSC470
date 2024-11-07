3778e68a9f673a932c636428ffee803e7fae0da2
Cora Heidinger <Cora.Heidinger@trojans.dsu.edu>
2024-11-07 12:21:49 -0600

    add get and post function for Create Account

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
+const UserModel=require("./Script/models/User")
+const path = require("path");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static("public"));
+app.use("/Script", express.static(path.join(__dirname, "Script")));
 
 app.get("/", function(req, res) {
     let contents = fs.readFileSync("./html/HelloWorld.html");
@@ -15,6 +18,18 @@ app.get("/", function(req, res) {
     res.send(contents);
     res.end();
 });
+
+app.get("/CreateAccount", function(req, res) {
+    let contents = fs.readFileSync("./html/CreateAccountPage.html");
+    res.header("Content-Type", "text/html");
+    res.status(200);
+    res.send(contents);
+    res.end();
+});
+
+app.post("/CreateAccount", function(req, res){
+    // Go back to login page here
+})
 
 const PORT = 8080;
 const HOST = '192.168.1.104'; //Server IP 192.168.1.104:8080
